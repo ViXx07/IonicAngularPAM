0cf2d9eed9385c6fadeb5dc5cba2cbad74d8d219
ViXx0 <vicen.perezb@duocuc.cl>
2024-09-28 09:51:28 -0300

    Configuración Base Firebase

--- SkeletonAPP/src/environments/environment.prod.ts
+++ SkeletonAPP/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyA3rcp9Gsaquh-zqQSxnyXHCPDOv5jEV58",
+    authDomain: "angularfirebaseproject-3becc.firebaseapp.com",
+    projectId: "angularfirebaseproject-3becc",
+    storageBucket: "angularfirebaseproject-3becc.appspot.com",
+    messagingSenderId: "993990866527",
+    appId: "1:993990866527:web:1c1102894ee8af9a414dea",
+    measurementId: "G-WZNP5V0NYZ"}
 };
--- SkeletonAPP/src/environments/environment.ts
+++ SkeletonAPP/src/environments/environment.ts
@@ -1,16 +1,12 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyA3rcp9Gsaquh-zqQSxnyXHCPDOv5jEV58",
+    authDomain: "angularfirebaseproject-3becc.firebaseapp.com",
+    projectId: "angularfirebaseproject-3becc",
+    storageBucket: "angularfirebaseproject-3becc.appspot.com",
+    messagingSenderId: "993990866527",
+    appId: "1:993990866527:web:1c1102894ee8af9a414dea",
+    measurementId: "G-WZNP5V0NYZ"}
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
