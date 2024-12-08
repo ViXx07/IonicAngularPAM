77c4436ee3c6b91f123b9c69abcb4f61f8c783af
ViXx07 <vicen.perezb@duocuc.cl>
2024-12-08 14:55:26 -0300

    interfaz de contacto

--- SkeletonAPP/src/app/pages/contacto/contacto.page.ts
+++ SkeletonAPP/src/app/pages/contacto/contacto.page.ts
@@ -1,8 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {Clipboard} from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.page.html',
   styleUrls: ['./contacto.page.scss'],
 })
-export class ContactoPage {}
+export class ContactoPage {
+
+  clip = inject(Clipboard);
+
+
+  contactos: any[]= [
+    {key: 'Whatsapp', value: '+123456789 '},
+    {key: 'Correo', value: 'correo@corre.ito'},
+    {key: 'Página Web', value: 'www.QueOpinas.cl'}
+  ]
+
+  getText(texto: string){
+    this.clip.copy(texto);
+  }
+}
